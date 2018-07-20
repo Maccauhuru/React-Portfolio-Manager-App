582599723d933bd180c7779a8b31150a23331060
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-20 17:20:49 -0500

    new component to render todo items

---
+++ src/Components/TodoItems.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class TodoItems extends Component {
+    render() {
+        return(
+            <li>
+            <strong>{this.props.todo.title}</strong> 
+            </li>
+        );
+        }
+    }
+
+     TodoItems.propTypes = {
+       todo : PropTypes.object
+ }
+   
+    export default TodoItems;
