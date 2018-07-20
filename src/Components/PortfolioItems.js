d7ad1dadbcca33ab7d18b1ffade0da0b9c151152
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-20 17:19:11 -0500

    added onDelete,and new PropTypes

--- src/Components/PortfolioItems.js
+++ src/Components/PortfolioItems.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class PortfolioItems extends Component {
+handleDeleteProject(id){
+    this.props.onDelete(id);
+}
+
     render() {
         return(
             <li>
-            <strong>{this.props.item.title}</strong> - {this.props.item.category}
+            <strong>{this.props.item.title}</strong> - {this.props.item.category}<a href="#" onClick={this.handleDeleteProject.bind(this,this.props.item.id)}>X</a>
             </li>
         );
         }
     }
+
+     PortfolioItems.propTypes = {
+       item : PropTypes.object,
+       onDelete  :  PropTypes.func,
+ }
    
     export default PortfolioItems;
